6886f5e91c965b65b625e37883ebf2cc2ed6df0c
Vinicius de Andrade <viniciusy62@gmail.com>
2023-02-28 19:39:01 -0300

    Update exercice03.js

---
+++ Js/exercice03.js
@@ -0,0 +1,17 @@
+//3º Pedir para o usuario digitar um numero;
+let num1 = prompt("Digite um número")
+//converter em numero inteiro;
+num1 = parseInt(num1)
+//pedir para o usuario digitar outro numero;
+let num2 = prompt("Digite outro número")
+//converter em numero inteiro;
+num2 = parseInt(num2)
+//mostrar a subtração concatenando com o texto "a subtração é:;
+alert("a subtração é: " + (num1 - num2))
+//mostrar a multiplicação concatenando com o texto "a multiplicação é: ...";
+alert("a multiplicação é: " + (num1 * num2))
+//mostrar a divisão concatenando com o texto "a divisão é: ...";
+alert("a divisão é: " + (num1 / num2))
+//mostrar a soma concatenando com o texto "a soma é: ..."
+alert("a soma é: " + (num1 + num2))
+
