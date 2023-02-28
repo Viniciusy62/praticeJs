f2a0d721e8febe83a08fd52f3863e72eca0a2377
Vinicius de Andrade <viniciusy62@gmail.com>
2023-02-28 19:39:47 -0300

    Update exercice04.js

---
+++ Js/exercice04.js
@@ -0,0 +1,7 @@
+//4º Pedir para o usuário digitar dois números e mostrar a média
+
+let num1 = Number(prompt("Digite um número"));
+let num2 = Number(prompt("Digite outro número"));
+let media = (num1 + num2) / 2;
+
+alert("A média é " + media);
