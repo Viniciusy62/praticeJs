7638d53bcd57f2f0b3659c24e4fe92d8965b5530
Vinicius de Andrade <viniciusy62@gmail.com>
2023-02-28 19:36:21 -0300

    Update exercice02.js

---
+++ Js/exercice02.js
@@ -0,0 +1,6 @@
+//2º Pedir para o usuário digitar dois números e mostrar a soma
+
+let num1 = Number(prompt("Digite um número"));
+let num2 = Number(prompt("Digite outro número"))
+let soma = num1 + num2
+let resultado = alert(`O primeiro número é ${num1}, o segundo número é ${num2} e a soma é ${soma}`)
