069605f50743507b57427d06313743094393bbcf
Vinicius de Andrade <viniciusy62@gmail.com>
2023-02-28 21:32:35 -0300

    Update exercice10.js

--- Js/exercice10.js
+++ Js/exercice10.js
@@ -1,6 +1,9 @@
 //Pedir para o usuário digitar um número e mostrar a tabuada desse número de 1 a 100.
 let number = Number(prompt('Digite um número qualquer'))
 
-for(i = 0; i <= 100; i++) {
-    document.write(`${number} x ${i} = ${number * i} <br>`) 
+for(i = 1; i <= 100; i++) {
+    document.write(`${number} x ${i} = ${number * i} <br>`)
+    if(i % 10 === 0) {
+        document.write(`---------------- <br>`)
+    }
 }
