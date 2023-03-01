0e46c1a594ba0626cd2c8dc6d59524a03cf53074
Vinicius de Andrade <viniciusy62@gmail.com>
2023-02-28 22:13:23 -0300

    Update exercice11.js

--- Js/exercice11.js
+++ Js/exercice11.js
@@ -1,3 +1,9 @@
 /*
 Para este exercício, mostre na tela todos os anos a partir de 1004 até o ano 2017, pulando de quatro em quatro anos.
 */
+let ano = 1004
+
+while(ano < 2023) {
+    document.write(`Ano: ${ano}<br>`)
+    ano += 4
+}
