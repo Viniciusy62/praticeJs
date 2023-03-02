04bcd85b2107b07c7c6b9569c09d9670a30d1877
Vinicius de Andrade <viniciusy62@gmail.com>
2023-03-02 16:52:18 -0300

    Update exercice11.js

--- Js/exercice11.js
+++ Js/exercice11.js
@@ -1,9 +1,15 @@
 /*
-Para este exercício, mostre na tela todos os anos a partir de 1004 até o ano 2017, pulando de quatro em quatro anos.
+Para este exercício, mostre na tela todos os anos a partir de 1004, os anos bissextos. Onde: Os anos bissextos são múltiplos de 4, não múltiplos de 100 (1900 não é bissexto) e múltiplos de 400 (2000 é bissexto).
 */
 let ano = 1004
 
 while(ano < 2023) {
-    document.write(`Ano: ${ano}<br>`)
+    if(ano % 4 == 0 && !(ano % 100 == 0) || ano % 400 == 0) {
+        document.write(`Ano: ${ano}<br>`)
+} else {
+    document.write(`Ano: ${ano} não é bissexto.<br>`)
+}
     ano += 4
 }
+
+
