ba181fee7c235a91fe5e2e226744abcce4bf28c8
Vinicius de Andrade <viniciusy62@gmail.com>
2023-03-07 16:30:32 -0300

    update exercice 12

--- Js/exercice12.js
+++ Js/exercice12.js
@@ -1,13 +1,23 @@
 let number = document.querySelector('#number')
 let verify = document.querySelector('#verify')
 let clean = document.querySelector('#clean')
+let result = document.querySelector('#result')
 
 function calculator() {
-    alert('teste')
+    let multiple = 0
+    if(number.value == "") {
+        result.textContent = "Digite um valor numérico para criar uma tabuada"
+    } else {
+        result.innerHTML = "Resultado: <br>"
+        for(i=0;i<=10;i++) {
+            multiple = number.value * i
+            result.innerHTML += `${number.value} x ${i} = ${multiple} <br>` 
+        }
+    }
 }
 
 function cleaner() {
-    alert('cleaner')
+    result.textContent = "Resultado:"
 }
 
 verify.addEventListener('click', calculator)
